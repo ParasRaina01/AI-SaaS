92614e7ea287bcbfd6c3301bc98a44693ce338bd
ParasRaina01 <90261069+ParasRaina01@users.noreply.github.com>
2023-07-23 21:41:04 +0530

    dashboard done

---
+++ components/navbar.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button"
+import MobileSideBar from "./mobile.sidebar";
+const Navbar = () => {
+    return (
+        <div className="flex items-center  p-4">
+            <MobileSideBar/>
+            <div className="flex w-full justify-end">
+                <UserButton afterSignOutUrl="/"/>
+            </div>
+        </div>
+    )
+}
+export default Navbar;
